90f58ff71bdab19432f965732fc67f8c3f4cd145
David Cornelius <davidcornelius15@gmail.com>
2020-01-24 10:37:15 +0700

    Echo David

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 echo "Init commit";
 echo "Source Tree 2 ";
+echo "Otp";
+echo "sarapan pagi";
+echo "sarapan pagi done";
+echo "sarapan pagi gitkraken";
 echo "richard";
 echo "david";
 echo "Tofit";
